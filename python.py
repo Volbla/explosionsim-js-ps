58e678b215d2b0b00723cca802dfe7604d170561
Volbla <smor_fran_margarinesien@hotmail.com>
2023-01-31 13:19:24 +0100

    Python implementation kept for posterity.

--- python.py
+++ python.py
@@ -1,14 +1,11 @@
-export const code = `
+# export const code = `
 from pyodide.ffi import to_js
 import numpy as np
 from itertools import product
 
 
-def array_dot(a:np.ndarray, b:np.ndarray) -> np.ndarray:
-	return np.einsum("...n, ...n", a, b)
-
 def array_dot2(a:np.ndarray) -> np.ndarray:
-	return array_dot(a, a)
+	return np.einsum("...n, ...n", a, a)
 
 
 cube_corners = np.array(
@@ -24,22 +21,14 @@ cube_origins = np.array(
 )
 squared_dist = array_dot2(cube_corners[None,:,:] + cube_origins[:,None,:])
 
-# currently unused
-# explosion_dirs = np.array([
-# 	xyz for xyz in product(range(16), repeat=3)
-# 	if 0 in xyz or 15 in xyz
-# 	], dtype=np.float32
-# ) - 7.5
-# explosion_dirs /= array_dot2(explosion_dirs)[:,None]
-
 
 def origins(radius:float):
-	close = np.any(squared_dist < radius ** 2, axis=1)
+	near = np.any(squared_dist < radius ** 2, axis=1)
 	far = np.any(squared_dist > radius ** 2, axis=1)
-	hole = cube_origins[close & far]
+	hole = cube_origins[near & far]
 	return to_js(hole.flatten())
 
 
 # The last expression is returned when pyodide runs the code.
 origins
-`
+# `
