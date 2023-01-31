ba5480c0e35ed47644bdcc3de81019fba7c7f8fd
Volbla <smor_fran_margarinesien@hotmail.com>
2023-01-31 13:18:45 +0100

    Remove unused. Minor jujing up.

--- arraylib.js
+++ arraylib.js
@@ -1,34 +1,27 @@
-
-
 function fdiv(a, b){
 	return Math.floor(a / b);
 }
 
-function arrayOp(func, arr){
-	return arr.map(vec =>
-		func(vec)
-	)
-}
-
-function dot2a(vec){
+function dot2(vec){
 	return vec.reduce((sum, el) => {
 		return sum + el ** 2
 	}, 0)
 }
 
 function vecSum(vec1, vec2){
-	return Array.from({length: 3}, (_, i) => {
-		return vec1[i] + vec2[i]
-	});
+	return Array.from({length: 3}, (_, i) =>
+		vec1[i] + vec2[i]
+	);
 }
 
 function cubeGrid(size, start = [0,0,0]) {
 	const [x, y, z] = size;
 	const [x0, y0, z0] = start;
 	let points = [];
+
 	for(let i=0; i < x * y * z; i++){
 		points.push([
-			fdiv(i, y * z) + x0,
+			fdiv(i, z * y) + x0,
 			fdiv(i, z) % y + y0,
 			i % z + z0
 		]);
@@ -46,10 +39,10 @@ export function touchesSphere(radius){
 	const sphere = origins.filter(vec => {
 		const
 		near = corners.some(corn =>
-			dot2a(vecSum(vec, corn)) < r2
+			dot2(vecSum(vec, corn)) < r2
 		),
 		far = corners.some(corn =>
-			dot2a(vecSum(vec, corn)) > r2
+			dot2(vecSum(vec, corn)) > r2
 		)
 		return near && far
 	})
